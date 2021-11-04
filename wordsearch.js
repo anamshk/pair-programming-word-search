145e4e37bce091500abf6742468ae4144a2019b6
Anam Shaikh <anam.shk@outlook.com>
2021-11-03 20:10:53 -0400

    commit to master

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  
+  let bigArr = [];
+  const rows = letters.length;
+  const cols = letters[0].length;
+  for (let j = 0; j < cols; j++) {
+    let smallArr = [];
+    for (let i = 0; i < rows; i++) {
+      smallArr.push(letters[i][j]);
+    }
+    bigArr.push(smallArr);
+  }
+  
+  const virtical = bigArr.map(ls => ls.join(''));
+  for (const l of virtical) {
+    if (l.includes(word)) return true;
+    else return false;
+  }
+  
+};
+
+module.exports = wordSearch;
